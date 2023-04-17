3255199c23c1e9db81fbe1d22798512575745441
Procile <114362384+Procile@users.noreply.github.com>
2023-04-16 21:34:07 -0500

    Add files via upload

---
+++ static/js/apps.js
@@ -0,0 +1,44 @@
+const button = document.getElementById("discord");
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https://discord.com");
+});
+
+const button = doucment.getElementById("snapchat");
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https://snapchat.com");
+});
+
+const button = doucment.getElementById("goku-to")
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https://goku.to");
+});
+
+const button = doucment.getElementById("instagram")
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https://instagram.com");
+});
+
+const button = doucment.getElementById("google")
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https://google.com");
+});
+
+const button = doucment.getElementById("twitter")
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https://twitter.com");
+});
+
+const button = doucment.getElementById("reddit")
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https://reddit.com");
+});
+
+const button = doucment.getElementById("textnow")
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https:/textnow.com");
+});
+
+const button = doucment.getElementById("tiktok")
+button.addEventListener("click", function() {
+  window.location.href = "https://" + location.hostname + __uv$config.prefix + __uv$config.encodeUrl("https:/tiktok.com");
+});
